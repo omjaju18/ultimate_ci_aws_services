95d6091aaca6a50de960e69ab42e2fbc454f1020
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2025-02-03 20:08:04 +0530

    add files for code deploy

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull omjaju18/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 omjaju18/simple-python-flask-app:latest
